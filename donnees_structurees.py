fb3f51032d3bac7f1c601d9220bce5e769574e52
LordTenebra <35447162+LordTenebra@users.noreply.github.com>
2020-11-23 14:11:41 +0100

    Add files via upload

---
+++ donnees_structurees.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+produit = {
+1:{"nom":"banane", "prix":4},
+2:{"nom":"Pomme","prix":2},
+3:{"nom":"Orange", "prix":1.5},
+4:{"nom":"Poire","prix":3}
+}
+prix_tot = int(0)
+while True:
+    try:
+        nb_produit = int(input("Combiens de produit :"))
+        for i in range(1, nb_produit+1) :
+            try:
+                ref = int(input("selectionner le produit : "))
+                if ref > 0 and ref < len(produit)+1:
+                    select = produit[ref]
+                    try:
+                        quant = int(input("combiens en voulez vous : "))
+                        if quant > 0:
+                            prix_fin_HT = float(select['prix']) * quant
+                            prix_tot += prix_fin_HT
+                        else:
+                            print("erreur : nombre inférieur à 1")
+                    except ValueError:
+                        print("veuillez entrer un entier")
+            except ValueError:
+                print("entrer un nombre entre 1 et 4")
+        print("Votre facture Hors taxes est de ", prix_tot)
+        if prix_tot > 200:
+            prix_tot -= ((prix_tot * 5) / 100)
+        print("Votre facture TTC est de ", prix_tot + ((prix_tot * 20) / 100))
+        break
+    except ValueError:
+        print("entrer un nombre")
