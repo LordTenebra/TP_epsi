bad5c99a8e648334aca34b2c077e7c1a2d1e8d04
LordTenebra <35447162+LordTenebra@users.noreply.github.com>
2020-11-23 11:53:59 +0100

    Add files via upload

--- caisse_vente.py
+++ caisse_vente.py
@@ -5,11 +5,14 @@ while True:
         nb_produit = int(input("Combiens de produit :"))
         for i in range(1, nb_produit+1) :
             try:
-                prix_HT = int(input("Entrer le prix : "))
-                prix_TTC = prix_HT + ((prix_HT * 20) / 100)
-                if (prix_TTC >= 200) :
-                    prix_TTC = prix_TTC - ((prix_TTC * 5) / 100)
-                print ("le produit", i, "a por valeur", prix_TTC)
+                prix_HT = float(input("Entrer le prix : "))
+                if(prix_HT <= float(0)):
+                    print("prix superieur a 0")
+                else:
+                    prix_TTC = prix_HT + ((prix_HT * 20) / 100)
+                    if (prix_TTC >= 200) :
+                        prix_TTC = prix_TTC - ((prix_TTC * 5) / 100)
+                    print ("le produit", i, "a por valeur", prix_TTC)
             except ValueError:
                 print("entrer un nombre")
         break
