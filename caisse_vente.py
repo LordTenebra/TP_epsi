3dd473268aca4defac9eb7d426e9be540c056f23
LordTenebra <35447162+LordTenebra@users.noreply.github.com>
2020-11-23 11:29:16 +0100

    Add files via upload

--- caisse_vente.py
+++ caisse_vente.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
-nb_produit = int(input("Combiens de produit :"))
-for i in range(1, nb_produit+1) :
-    prix_HT = int(input("Entrer le prix : "))
-    prix_TTC = prix_HT + ((prix_HT * 20) / 100)
-    if (prix_TTC >= 200) :
-        prix_TTC = prix_TTC - ((prix_TTC * 5) / 100)
-    print ("le produit", i, "a por valeur", prix_TTC)
+while True:
+    try:
+        nb_produit = int(input("Combiens de produit :"))
+        for i in range(1, nb_produit+1) :
+            try:
+                prix_HT = int(input("Entrer le prix : "))
+                prix_TTC = prix_HT + ((prix_HT * 20) / 100)
+                if (prix_TTC >= 200) :
+                    prix_TTC = prix_TTC - ((prix_TTC * 5) / 100)
+                print ("le produit", i, "a por valeur", prix_TTC)
+            except ValueError:
+                print("entrer un nombre")
+        break
+    except ValueError:
+        print("entrer un nombre")
