07a7b9eef1dfc3ee456a2c20a8353662587c6e6f
LordTenebra <35447162+LordTenebra@users.noreply.github.com>
2021-01-12 15:54:50 +0100

    Add files via upload

--- dossier_region/region.py
+++ dossier_region/region.py
@@ -1,7 +1,32 @@
 import csv
 import os
-with open('departements-france.csv', newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+import shutil
+cible = input("selectionner le fichier cible \n")
+recup = ""
+if cible != "":
+    os.makedirs(cible, exist_ok=True)
+    os.chdir(cible)
+    recup = "../"
+with open(recup + 'departements-france.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    count = 0
+    region = []
+    suppr = 0
     for row in spamreader:
-        #os.makedirs(row[0], exist_ok=True)
-        print(row[0])
+        if count == 0:
+            count = 1
+        else:
+            os.makedirs(row[3], exist_ok=True)
+            for rec in region:
+                if rec == row[3]:
+                    suppr = 1
+            if suppr == 0:
+                vider = input("Voulez vous vider le dossier " + row[3] + " ? y / n \n")
+                if vider == "y":
+                    shutil.rmtree(row[3])
+                    os.makedirs(row[3], exist_ok=True)
+            region.append(row[3])
+            suppr = 0
+            os.chdir(row[3])
+            os.makedirs(row[1], exist_ok=True)
+            os.chdir('../')
